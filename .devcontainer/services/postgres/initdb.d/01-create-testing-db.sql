1c1287da1ee8dd9e0763a43460abc3ad671c400e
Mitsuhito Fujita <mail@mitsuhito.dev>
2025-04-13 08:27:41 +0000

    feat: add postgresql with docker compose

---
+++ .devcontainer/services/postgres/initdb.d/01-create-testing-db.sql
@@ -0,0 +1,3 @@
+--
+CREATE DATABASE nextjs_app_testing ENCODING 'UTF-8' LC_COLLATE 'C' LC_CTYPE 'C';
+GRANT ALL PRIVILEGES ON DATABASE nextjs_app_testing TO nextjs_app;
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # typescript-nextjs-vercel
+
+### Docker Desctopのpostgresコンテナからターミナルでpostgresに接続する
+
+```
+psql -h localhost -p 5432 -U nextjs_app nextjs_app_local
+```
